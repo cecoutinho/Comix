5413e48bbaa5e7abd4491b61a23511a8d46814da
Carlos Coutinho <carlos.coutinho@caixamagica.pt>
2023-07-20 09:43:37 +0100

    Versão atualizada, gera toda uma coleção, mas tem o problema que com isso faz demasiados pedidos a gocomics.com e com isso o IP fica bloqueado no site, por isso é melhor alterar para só gerar ano a ano.

--- src/pt/cackossoft/Comix.java
+++ src/pt/cackossoft/Comix.java
@@ -34,32 +34,32 @@ public class Comix {
             "Ask Shagg (Sundays)",
             "Kid Spot (except Sundays)" };
     private final String[] fComicUrls = {
-            "http://www.gocomics.com/adamathome",
-            "http://www.gocomics.com/forbetterorforworse",
-            "http://www.gocomics.com/pickles",
-            "http://www.gocomics.com/ben",
-            "http://www.gocomics.com/baldo",
-            "http://www.gocomics.com/doonesbury",
-            "http://www.gocomics.com/redandrover",
-            "http://www.gocomics.com/garfield",
-            "http://www.gocomics.com/calvinandhobbes",
-            "http://www.gocomics.com/dilbert-classics",
-            "http://www.gocomics.com/nonsequitur",
-            "http://www.gocomics.com/theargylesweater",
-            "http://www.gocomics.com/realitycheck",
-            "http://www.gocomics.com/speedbump",
-            "http://www.gocomics.com/ripleysbelieveitornot",
-            "http://www.gocomics.com/trivquiz",
-            "http://www.gocomics.com/truth-facts",
-            "http://www.gocomics.com/biographic",
-            "http://www.gocomics.com/magicinaminute",
-            "http://www.gocomics.com/askshagg",
-            "http://www.gocomics.com/kidspot" };
+            "https://www.gocomics.com/adamathome",
+            "https://www.gocomics.com/forbetterorforworse",
+            "https://www.gocomics.com/pickles",
+            "https://www.gocomics.com/ben",
+            "https://www.gocomics.com/baldo",
+            "https://www.gocomics.com/doonesbury",
+            "https://www.gocomics.com/redandrover",
+            "https://www.gocomics.com/garfield",
+            "https://www.gocomics.com/calvinandhobbes",
+            "https://www.gocomics.com/dilbert-classics",
+            "https://www.gocomics.com/nonsequitur",
+            "https://www.gocomics.com/theargylesweater",
+            "https://www.gocomics.com/realitycheck",
+            "https://www.gocomics.com/speedbump",
+            "https://www.gocomics.com/ripleysbelieveitornot",
+            "https://www.gocomics.com/trivquiz",
+            "https://www.gocomics.com/truth-facts",
+            "https://www.gocomics.com/biographic",
+            "https://www.gocomics.com/magicinaminute",
+            "https://www.gocomics.com/askshagg",
+            "https://www.gocomics.com/kidspot" };
 
     private String baseUrl;
     private String comicId;
     private String comicName;
-    private String minDate;
+    private String minDate = "2023/01/01";
     private String maxDate;
     private String description;
 
@@ -74,42 +74,49 @@ public class Comix {
             final String fComicUrl = fComicUrls[ArrayUtils.indexOf(fComicNames, fComicName)];
             getPageInfo(fComicUrl);
 
-            // Get the start and end dates
-            String startDate = minDate;
-            String endDate = startDate.substring(0, 4) + "/12/31";
-
             final SimpleDateFormat fStr2Date = new SimpleDateFormat("yyyy/MM/dd");
             final SimpleDateFormat fDate2Str = new SimpleDateFormat("yyyy/MM/dd");
             final SimpleDateFormat fDate2Print = new SimpleDateFormat("EEEE, yyyy-MM-dd");
             Calendar lCalendarStt = Calendar.getInstance();
             Calendar lCalendarEnd = Calendar.getInstance();
+            Calendar lCalendar = Calendar.getInstance();
+            Calendar lCalendarMaxDate = Calendar.getInstance();
 
             try {
-                lCalendarStt.setTime(fStr2Date.parse(startDate));   // Start date
-                lCalendarEnd.setTime(fStr2Date.parse(endDate));     // End date
+                lCalendarStt.setTime(fStr2Date.parse(minDate));   // Start date
+                lCalendarMaxDate.setTime(fStr2Date.parse(maxDate)); // Max date
+                lCalendarMaxDate.add(Calendar.DATE, 1); // Need to add 1 day to the final date so that it cycles through the final date inclusive
             } catch (ParseException e) {
                 e.printStackTrace();
             }
 
             BufferedWriter out;
             try {
-                final String lPageTitle = "cackos-comics-" + comicId + "-" + startDate.replaceAll("/", "") + "_" + endDate.replaceAll("/", "");
-                out = new BufferedWriter(new FileWriter(lPageTitle + "-urls.htm"));
-                out.write("<html><head><title>" + lPageTitle + "</title></head><body><b>Cackos Comics: " + comicName + "<br>From: " +
-                        fDate2Print.format(lCalendarStt.getTime()) + "<br>To: " + fDate2Print.format(lCalendarEnd.getTime()) + "</b><br><br>" + description);
-                lCalendarEnd.add(Calendar.DATE, 1); // Need to add 1 day to the final date so that it cycles through the final date inclusive
-                for (; lCalendarStt.before(lCalendarEnd); lCalendarStt.add(Calendar.DATE, 1)) {
-                    String lTargetUrl = baseUrl + "/" + comicId + "/" + fDate2Str.format(lCalendarStt.getTime());
-                    String[] lComicUrls = WebPageReader.getComicPictureURL(lTargetUrl);
-                    System.out.println(fDate2Print.format(lCalendarStt.getTime()) + ": " + lComicUrls[0] + " + " + lComicUrls[1]);
-                    String lComicUrl = null == lComicUrls[1] ? lComicUrls[0] : lComicUrls[1];
-                    if (null != lComicUrl) {
-                        out.write("<hr>Comic: " + comicName + " - Date: " + fDate2Print.format(lCalendarStt.getTime()) + "<br><a href=" + lTargetUrl + "><img src=\"" + lComicUrl + "\"></a>");
+                for (; lCalendarStt.before(lCalendarMaxDate);) {
+                    lCalendarEnd.setTime(lCalendarStt.getTime());     // End date
+                    lCalendarEnd.add(Calendar.YEAR, 1);      // Add 1 Year
+                    lCalendarEnd.set(Calendar.MONTH, 0);        // 0 = January
+                    lCalendarEnd.set(Calendar.DAY_OF_MONTH, 1); // new year
+                    lCalendarEnd.setTime((lCalendarMaxDate.before(lCalendarEnd) ? lCalendarMaxDate : lCalendarEnd).getTime());
+                    lCalendarEnd.add(Calendar.DATE, -1);
+                    String lPageTitle = "cackos-comics-" + comicId + "-" + fDate2Str.format(lCalendarStt.getTime()).replaceAll("/", "") + "_" + fDate2Str.format(lCalendarEnd.getTime()).replaceAll("/", "");
+                    out = new BufferedWriter(new FileWriter(lPageTitle + "-urls.htm"));
+                    out.write("<html><head><title>" + lPageTitle + "</title></head><body><b>Cackos Comics: " + comicName + "<br>From: " +
+                            fDate2Print.format(lCalendarStt.getTime()) + "<br>To: " + fDate2Print.format(lCalendarEnd.getTime()) + "</b><br><br>" + description);
+                    lCalendarEnd.add(Calendar.DATE, 1); // Need to add 1 day to the final date so that it cycles through the final date inclusive
+                    for (; lCalendarStt.before(lCalendarEnd); lCalendarStt.add(Calendar.DATE, 1)) {
+                        String lTargetUrl = baseUrl + "/" + comicId + "/" + fDate2Str.format(lCalendarStt.getTime());
+                        String[] lComicUrls = WebPageReader.getComicPictureURL(lTargetUrl);
+                        //                    System.out.println(fDate2Print.format(lCalendarStt.getTime()) + ": " + lComicUrls[0] + " + " + lComicUrls[1]);
+                        String lComicUrl = null == lComicUrls[1] ? lComicUrls[0] : lComicUrls[1];
+                        if (null != lComicUrl) {
+                            out.write("<hr>Comic: " + comicName + " - Date: " + fDate2Print.format(lCalendarStt.getTime()) + "<br><a href=" + lTargetUrl + "><img src=\"" + lComicUrl + "\"></a>");
+                        }
                     }
+                    out.write("</body></html>");
+                    out.flush();
+                    out.close();
                 }
-                out.write("</body></html>");
-                out.flush();
-                out.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- src/pt/cackossoft/WebPageReader.java
+++ src/pt/cackossoft/WebPageReader.java
@@ -4,19 +4,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import javax.net.ssl.HttpsURLConnection;
+
 /**
  * Created by cecoutinho on 11-08-2014.
  */
 public class WebPageReader {
     public static String[] getComicPictureURL(String aComicUrl) {
         final String[] fResult = new String[2];
-        final String fWebPage = readWebPage(aComicUrl);
+        String fWebPage = readWebPage(aComicUrl);
         if (null == fWebPage) {
             return fResult;
         }
 
         // Search for the comic date in the page. Is it equal to the requested date?
-        String lSearchString = "<meta name=\"twitter:url\" content=\"";
+        String lSearchString = "<meta property=\"og:url\" content=\"";
         int lIndexStt = fWebPage.indexOf(lSearchString);
         if (lIndexStt < 0) {
             return fResult;
@@ -30,7 +34,7 @@ public class WebPageReader {
         }
 
         // Search for the comic pictures URIs (first one is usually the smaller, the second is the real sized one (zoomed))
-        lSearchString = "class=\"strip\" src=\"";
+        lSearchString = "<meta property=\"og:image\" content=\"";
         lIndexStt = fWebPage.indexOf(lSearchString);
         if (lIndexStt < 0) {
             return fResult;
@@ -53,13 +57,13 @@ public class WebPageReader {
 
     public static String[] getComicInfo(String aComicUrl) {
         final String[] fResult = new String[6];
-        final String fWebPage = readWebPage(aComicUrl);
+        String fWebPage = readWebPage(aComicUrl);
         if (null == fWebPage) {
             return fResult;
         }
 
         // Search for the Twitter tag in the page.
-        String lSearchString = "<meta name=\"twitter:url\" content=\"";
+        String lSearchString = "link rel=\"canonical\" href=\"";
         int lIndexStt = fWebPage.indexOf(lSearchString);
         if (lIndexStt < 0) {
             return fResult;
@@ -69,50 +73,29 @@ public class WebPageReader {
             return fResult;
         }
         String lResultString = fWebPage.substring(lIndexStt + lSearchString.length(), lIndexEnd);
-        // Expected format: <BaseURL>/<comicId>/yyyy/MM/dd
-        lResultString = lResultString.substring(0, lResultString.length() - "/yyyy/MM/dd".length());
+        // Expected format: <BaseURL>/<comicId>, e.g., https://www.gocomics.com/forbetterorforworse
+//        lResultString = lResultString.substring(0, lResultString.length() - "/yyyy/MM/dd".length());
         lIndexStt = lResultString.lastIndexOf("/");
+        // Expected value example: fResult[0] = "https://www.gocomics.com"
         fResult[0] = lResultString.substring(0, lIndexStt);
+        // Expected value example: fResult[1] = "forbetterorforworse"
         fResult[1] = lResultString.substring(lIndexStt + 1);
 
         // Search for the Comic Name
-        lSearchString = "<meta name=\"twitter:title\" content=\"";
+        lSearchString = "<title>Today on ";
         lIndexStt = fWebPage.indexOf(lSearchString);
         if (lIndexStt < 0) {
             return fResult;
         }
-        lIndexEnd = fWebPage.indexOf(",", lIndexStt + lSearchString.length());
+        lIndexEnd = fWebPage.indexOf("</title>", lIndexStt + lSearchString.length());
         if (lIndexEnd < 0) {
             return fResult;
         }
+        // Expected value example: fResult[2] = "For Better or For Worse - Comics by Lynn Johnston - GoComics"
         fResult[2] = fWebPage.substring(lIndexStt + lSearchString.length(), lIndexEnd);
 
-        // Search for the Description of the Comic (the search string appears twice, the good description comes after the Twitter definition)
-        lSearchString = "<meta name=\"description\" content=\"";
-        lIndexStt = fWebPage.indexOf(lSearchString, lIndexEnd);
-        if (lIndexStt < 0) {
-            return fResult;
-        }
-        lIndexEnd = fWebPage.indexOf("\"/>", lIndexStt + lSearchString.length());
-        if (lIndexEnd < 0) {
-            return fResult;
-        }
-        fResult[5] = fWebPage.substring(lIndexStt + lSearchString.length(), lIndexEnd);
-
-        // Search for the Minimum Date of the Comic
-        lSearchString = "minDate: \"";
-        lIndexStt = fWebPage.indexOf(lSearchString);
-        if (lIndexStt < 0) {
-            return fResult;
-        }
-        lIndexEnd = fWebPage.indexOf("\"", lIndexStt + lSearchString.length());
-        if (lIndexEnd < 0) {
-            return fResult;
-        }
-        fResult[3] = fWebPage.substring(lIndexStt + lSearchString.length(), lIndexEnd);
-
         // Search for the Maximum Date of the Comic
-        lSearchString = "maxDate: \"";
+        lSearchString = "primary\" href=\"/" + fResult[1] + "/";
         lIndexStt = fWebPage.indexOf(lSearchString);
         if (lIndexStt < 0) {
             return fResult;
@@ -123,29 +106,63 @@ public class WebPageReader {
         }
         lResultString = fWebPage.substring(lIndexStt + lSearchString.length(), lIndexEnd);
         if (!lResultString.equals("c")) {
+            // Expected value example: fResult[1] = "2023/07/18"
             fResult[4] = lResultString;
         }
+
+        // Search for the Minimum Date of the Comic
+        // The pattern happens twice, first with the current date and second with the minimum date
+        lIndexStt = fWebPage.indexOf(lSearchString, lIndexStt + lSearchString.length());
+        if (lIndexStt < 0) {
+            return fResult;
+        }
+        lIndexEnd = fWebPage.indexOf("\"", lIndexStt + lSearchString.length());
+        if (lIndexEnd < 0) {
+            return fResult;
+        }
+        // Expected value example: fResult[3] = "1979/09/10"
+        fResult[3] = fWebPage.substring(lIndexStt + lSearchString.length(), lIndexEnd);
+
+        // Read DESCRIPTION
+        fWebPage = readWebPage(aComicUrl + "/about");
+        if (null == fWebPage) {
+            return fResult;
+        }
+
+        // Search for the Description of the Comic (the search string appears twice, the good description comes after the Twitter definition)
+        lSearchString = "</h1>    <div class=\"content-section\">";
+        lIndexStt = fWebPage.indexOf(lSearchString);
+        if (lIndexStt < 0) {
+            return fResult;
+        }
+        lIndexEnd = fWebPage.indexOf("</p>", lIndexStt + lSearchString.length());
+        if (lIndexEnd < 0) {
+            return fResult;
+        }
+        fResult[5] = fWebPage.substring(lIndexStt + lSearchString.length(), lIndexEnd).trim();
 
         return fResult;
     }
 
     private static String readWebPage(String aUrl) {
-        StringBuilder sb = new StringBuilder();
+        String result = "";
         try {
-            InputStream in = new URL(aUrl).openStream();
-            byte[] buffer = new byte[256];
-            while (true) {
-                int byteRead = in.read(buffer);
-                if (byteRead == -1)
-                    break;
-                for (int i = 0; i < byteRead; i++){
-                    sb.append((char)buffer[i]);
-                }
+            URL myurl = new URL(aUrl);
+            HttpsURLConnection con = (HttpsURLConnection) myurl.openConnection();
+            con.setRequestProperty ( "User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0" );
+            InputStream ins = con.getInputStream();
+            InputStreamReader isr = new InputStreamReader(ins);
+            BufferedReader in = new BufferedReader(isr);
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                result += inputLine;
             }
+            in.close();
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
-        return sb.toString();
+        return result;
     }
 }
